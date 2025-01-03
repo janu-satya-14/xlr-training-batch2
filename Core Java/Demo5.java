66f4e75ad18df552e9660fe78114b27660d923b6
janu-satya-14 <2311CS010342@mallareddyuniversity.ac.in>
2025-01-03 09:55:54 +0530

    User authentication program

---
+++ Core Java/Demo5.java
@@ -0,0 +1,11 @@
+public class Demo5 {
+    public static void main(String args[])
+    {
+        String username= "user001";
+        String password= "user_001";
+        while((username == "user001") && (password == "user_001")) {
+            System.out.println("Hello user!Happy New Year!");
+            break;
+    }
+}
+}
