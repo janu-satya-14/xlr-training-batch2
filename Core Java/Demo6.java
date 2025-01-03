bb384b4b3e52d0c3f26a106a6038e3618562d32f
janu-satya-14 <2311CS010342@mallareddyuniversity.ac.in>
2025-01-03 10:13:39 +0530

    Hello Jhansi

---
+++ Core Java/Demo6.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class User_Authentication {
+    public static void main(String args[])
+    {
+        String username= "janu-satya-14";
+        String password= "janu@14";
+        while((username == "janu-satya-14") && (password == "janu@14"))
+        {
+            System.out.println("Hello Jhansi welcome to Malla Reddy University!");
+            break;
+    }
+}
+}
