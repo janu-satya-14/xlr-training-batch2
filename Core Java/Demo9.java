6076d6f9c145fa3ae7ffabe7d9cabffff2f833e1
janu-satya-14 <2311CS010342@mallareddyuniversity.ac.in>
2025-01-09 22:20:08 +0530

    name,accno,balance

--- Core Java/Demo9.java
+++ Core Java/Demo9.java
@@ -1,12 +1,29 @@
-public class Demo9 {
-    public static void main(String[] args)
-    {
-        int radius;
-        double pi = 3.142, area;
-        radius = 5;
-        // calculating the area of the circle
-        area = pi * radius * radius;
-        // printing the area of the circle
-        System.out.println("Area of circle is :" + area);
-    }
+import java.util.Scanner;
+public class Demo9{
+        Scanner scanner= new Scanner(System.in);
+        String customername(){
+            System.out.println("Enter the name:");
+            String name=scanner.next();
+            return name;
+        }
+        String accno(){
+            System.out.println("Acc no:");
+            String accno=scanner.next();
+            return accno;
+        }
+        String balance(){
+            System.out.println("Balance amount:");
+            String bal=scanner.next();
+            return bal;
+        }
+        public static void main(String[] args) {
+            Demo9 ob=new Demo9();
+            String name=ob.customername();
+            String accno=ob.accno();
+            String bal=ob.balance();
+            System.out.println("The name of the customer:"+name);
+            System.out.println("The accno provided:"+accno);
+            System.out.println("The balance amount is:"+bal);
+        }
+
 }
