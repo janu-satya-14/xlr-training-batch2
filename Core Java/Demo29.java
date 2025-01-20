b03118c0deb3db13f5ff4fe98aee7d2c3124f822
janu-satya-14 <2311CS010342@mallareddyuniversity.ac.in>
2025-01-20 09:41:04 +0530

    array of 10 friends

---
+++ Core Java/Demo29.java
@@ -0,0 +1,9 @@
+public class Demo29 {
+    public static void main(String[] args) {
+        String[] friends = {"Jhansi", "Priyaaaa", "Anuuuu", "Kavitaaaa", "Chandamamaa", "Snehithaaa", "Pranathiiii", "Sindhuu", "Phani", "Tejaswi"};
+        System.out.println("List of friends:");
+        for (String friend : friends) {
+            System.out.println(friend);
+        }
+    }
+}
