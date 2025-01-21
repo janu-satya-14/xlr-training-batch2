e8bbd5a89efe5bde4b9142d297a30d03fc18c964
janu-satya-14 <2311CS010342@mallareddyuniversity.ac.in>
2025-01-21 10:27:09 +0530

    hashset modified code

--- Core Java/Demo031.java
+++ Core Java/Demo031.java
@@ -1,5 +1,13 @@
 // Demo031.java Hash Set - Collections  
 import java.util.HashSet;
+public class Demo031 {
+    public static void main(String[] args) {
+        System.out.println("hello try");
+        HashSetDemo1 hsobj = new HashSetDemo1();
+        hsobj.HSetMethod();
+         }
+}
+
 class HashSetDemo1 {
 	void HSetMethod() {
 		//We can add any type of element here...
@@ -15,12 +23,5 @@ class HashSetDemo1 {
 			System.out.println(x);
 		}
 	}
-}
-public class Demo031 {
-    public static void main(String[] args) {
-        System.out.println("hello try");
-        HashSetDemo1 hsobj = new HashSetDemo1();
-        hsobj.HSetMethod();
-         }
 }
 
