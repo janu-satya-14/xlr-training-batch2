81167ec5379eef108221772f4d6edddf5080bf25
janu-satya-14 <2311CS010342@mallareddyuniversity.ac.in>
2025-01-26 21:31:49 +0530

    Inheritance

---
+++ Core Java/Demo011.java
@@ -0,0 +1,21 @@
+// wap to create inheritance - class name Parent and Sub Class name Child
+public class Demo011 {
+    public static void main(String[] args) {        
+        Parent pobj = new Parent();
+        pobj.parentDetails();
+        Child cobj = new Child();
+        cobj.childDetails();
+        }
+}
+class Parent{
+    public String Name = "Hitler";
+    void parentDetails(){
+        System.out.println("parents name is "+ Name);
+    }
+}
+class Child extends Parent{
+    String Son = "Jean Marie";
+    void childDetails(){
+        System.out.println("Sons full name is "+super.Name+". "+Son);
+    }
+}
